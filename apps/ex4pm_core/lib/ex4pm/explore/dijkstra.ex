c45d05dcc4928aced0b21eba15154af0cabfa025
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-23 20:22:28 -0700

    fix(explore): terminate Dijkstra on empty MapSet

--- apps/ex4pm_core/lib/ex4pm/explore/dijkstra.ex
+++ apps/ex4pm_core/lib/ex4pm/explore/dijkstra.ex
@@ -5,15 +5,20 @@ defmodule Ex4pm.Explore.Dijkstra do
     visit(MapSet.new(nodes), edges, dist)
   end
 
-  defp visit(unvisited, _edges, dist) when map_size(unvisited) == 0, do: dist
   defp visit(unvisited, edges, dist) do
-    u = Enum.min_by(unvisited, fn n -> rank(Map.fetch!(dist, n)) end)
-    du = Map.fetch!(dist, u)
-    dist = Enum.reduce(edges, dist, fn
-      {^u, v, w}, acc when w >= 0 -> relax(acc, v, du, w)
-      _, acc -> acc
-    end)
-    visit(MapSet.delete(unvisited, u), edges, dist)
+    if MapSet.size(unvisited) == 0 do
+      dist
+    else
+      u = Enum.min_by(unvisited, fn n -> rank(Map.fetch!(dist, n)) end)
+      du = Map.fetch!(dist, u)
+      dist =
+        Enum.reduce(edges, dist, fn
+          {^u, v, w}, acc when w >= 0 -> relax(acc, v, du, w)
+          _, acc -> acc
+        end)
+
+      visit(MapSet.delete(unvisited, u), edges, dist)
+    end
   end
 
   defp relax(dist, _v, :infinity, _w), do: dist
@@ -25,6 +30,7 @@ defmodule Ex4pm.Explore.Dijkstra do
       _ -> dist
     end
   end
+
   defp rank(:infinity), do: 1.0e300
   defp rank(v), do: v
 end
