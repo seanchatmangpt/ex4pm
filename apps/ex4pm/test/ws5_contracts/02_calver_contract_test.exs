a14ae02535a9465098e8c4cdcb52b9134027a2c2
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 18:54:55 -0700

    test: guard ex4pm CalVer release identity

---
+++ apps/ex4pm/test/ws5_contracts/02_calver_contract_test.exs
@@ -0,0 +1,7 @@
+defmodule Ex4pm.WS5.CalverContractTest do
+  use ExUnit.Case, async: true
+  @mix Path.expand("../../../../mix.exs", __DIR__) |> File.read!()
+  test "umbrella release identity remains on the admitted CalVer" do
+    assert @mix =~ ~s(version: "26.8.22")
+  end
+end
