463c4f49097c6f3fc7833277af7f04c7a082db2c
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 20:05:27 -0700

    test: guard runtime app release membership

---
+++ apps/ex4pm_core/test/ws5_contracts/11_release_runtime_test.exs
@@ -0,0 +1,7 @@
+defmodule Ex4pmCore.WS5.ReleaseRuntimeTest do
+  use ExUnit.Case, async: true
+  test "release keeps runtime application permanent" do
+    mix = File.read!(Path.expand("../../../../mix.exs", __DIR__))
+    assert mix =~ "ex4pm_runtime: :permanent"
+  end
+end
